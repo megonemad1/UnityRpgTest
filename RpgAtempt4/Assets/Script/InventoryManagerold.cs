2d61be94975f87237cb138c7e9cedb072ea94694
rhys <rhys.puddephatt@btinternet.com>
2016-06-19 19:24:26 +0100

    gits messing up

---
+++ RpgAtempt4/Assets/Script/InventoryManagerold.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+
+public class InventoryManagerold : MonoBehaviour
+{
+    public GameObject InventoryPanel, SlotPanel, InventorySlot, InventoryItem;
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+}
+//    HandManager hand;
+//    public int InventoryWidth = 9;
+//    public int InventoryHight = 6;
+//    public oldItem[] inventory;
+//    [HideInInspector]
+//    [SerializeField]
+//    int selected_item;
+//    [ExposeProperty]
+//    public int SelectedItem
+//    {
+//        get
+//        {
+//            return selected_item;
+//        }
+//        set
+//        {
+
+//            if (value < InventoryHight * InventoryWidth && value >= -1)
+//            {
+//                selected_item = value;
+//            }
+
+//            else
+//            {
+//                Debug.Log("invalid inventory slot: " + value);
+//                selected_item = -1;
+//            }
+
+//        }
+//    }
+//    public oldItem[] GetAllItemsOfType(ItemType itemtype)
+//    {
+
+//        return Array.FindAll(inventory, (oldItem item) => { return item.GetItemTypes().Contains(itemtype); });
+//    }
+//    public bool addItem(oldItem item)
+//    {
+//        int index = Array.FindIndex(inventory, (oldItem i) => { return i == null; });
+//        if (index > -1)
+//        {
+//            SetInventoryPosition(index, item);
+//            return true;
+//        }
+//        return false;
+//    }
+//    public oldItem GetSelected()
+//    {
+//        if (selected_item > -1)
+//            return GetInventoryPosition(selected_item);
+//        else
+//            return null;
+//    }
+
+//    public InventoryManager SwapItems(int InventorySlotA, int InventorySlotB)
+//    {
+//        oldItem tempA = GetInventoryPosition(InventorySlotA);
+//        oldItem tempB = GetInventoryPosition(InventorySlotB);
+
+//        SetInventoryPosition(InventorySlotA, tempB);
+//        SetInventoryPosition(InventorySlotB, tempA);
+//        return this;
+//    }
+//    public InventoryManager SetInventoryPosition(int InventorySlotX, int InventorySlotY, oldItem item)
+//    {
+
+//        return SetInventoryPosition(InventorySlotX * InventoryWidth + InventorySlotY, item);
+//    }
+//    public InventoryManager SetInventoryPosition(int InventorySlot, oldItem item)
+//    {
+//        if (InventorySlot < inventory.Length && InventorySlot >= 0)
+//        {
+//            inventory[InventorySlot] = item;
+//        }
+//        else
+//        {
+//            Debug.Log("invalid set inventory slot: " + InventorySlot + "on item " + item);
+//        }
+//        return this;
+//    }
+//    public oldItem GetInventoryPosition(int InventorySlotX, int InventorySlotY)
+//    {
+//        return GetInventoryPosition(InventorySlotX * InventoryWidth + InventorySlotY);
+//    }
+//    public oldItem GetInventoryPosition(int InventorySlot)
+//    {
+//        if (InventorySlot < inventory.Length && InventorySlot >= 0)
+//        {
+//            return inventory[InventorySlot];
+//        }
+//        else
+//        {
+//            Debug.Log("invalid set inventory slot: " + InventorySlot);
+//            return null;
+//        }
+//    }
+//    // Use this for initialization
+//    void Start()
+//    {
+//        inventory = new oldItem[InventoryWidth * InventoryHight];
+//        SelectedItem = -1;
+
+//        hand = GetComponent<HandManager>();
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//}
+//[CustomEditor(typeof(InventoryManager))]
+//public class InventoryManagerEditor : Editor
+//{
+
+
+//    InventoryManager m_Instance;
+//    PropertyField[] m_fields;
+
+
+//    public void OnEnable()
+//    {
+//        m_Instance = target as InventoryManager;
+//        m_fields = ExposeProperties.GetProperties(m_Instance);
+//    }
+
+//    public override void OnInspectorGUI()
+//    {
+
+//        if (m_Instance == null)
+//            return;
+
+//        this.DrawDefaultInspector();
+
+//        ExposeProperties.Expose(m_fields);
+
+//    }
+//}
---
+++ RpgAtempt4/Assets/Script/ItemDataBase.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using LitJson;
+using System.IO;
+public class ItemDataBase : MonoBehaviour
+{
+    private Dictionary<string, Item> ItemDatabase = new Dictionary<string, Item>();
+    JsonData itemData;
+    void Start()
+    {
+        string filePath = Application.dataPath + "/StreamingAssets/Items.json";
+        if (File.Exists(filePath))
+            itemData = JsonMapper.ToObject(File.ReadAllText(filePath));
+        else
+        {
+            Debug.LogError("NoJasonFile in system: " + filePath);
+            return;
+        }
+    }
+    public Item getItemByID(string id)
+    {
+        Item ritem;
+        if (!ItemDatabase.TryGetValue(id, out ritem))
+            Debug.LogError("id: " + id + " not found");
+        return ritem;
+    }
+    void ConstructItemDataBase()
+    {
+        if (itemData == null)
+        {
+            Debug.LogError("ItemData Null, ItemDataBaseNotLoaded");
+            return;
+        }
+        foreach (JsonData item in itemData)
+        {
+            Item tempItem = new Item(item);
+            ItemDatabase.Add(tempItem.id, tempItem);
+        }
+    }
+}
+public class Item
+{
+
+    public string id { get; set; }
+    public string title { get; set; }
+    public string discription { get; set; }
+    public int value { get; set; }
+    public bool stackable { get; set; }
+    public int rarity { get; set; }
+    public string[] types { get; set; }
+    public float damage { get; set; }
+    public float armorclass { get; set; }
+    public float strength { get; set; }
+    public float constitution { get; set; }
+    public float dexterity { get; set; }
+    public float intelligence { get; set; }
+    public float wisdom { get; set; }
+    public float charisma { get; set; }
+    public Sprite sprite { get; set; }
+    public Item()
+    {
+        id = "";
+    }
+
+    public Item(JsonData itemData)
+    {
+        try
+        {
+            this.id = itemData["id"].ToString();
+            this.title = itemData["title"].ToString();
+            this.discription = itemData["discription"].ToString();
+
+            this.value = (int)itemData["value"];
+            this.rarity = (int)itemData["rarity"];
+
+            this.stackable = (bool)itemData["stackable"];
+
+            this.types = new string[itemData["types"].Count];
+            for (int i = 0; i < this.types.Length; i++)
+                this.types[i] = itemData["types"][i].ToString();
+
+            this.damage = (float)itemData["stats"]["damage"];
+            this.armorclass = (float)itemData["stats"]["armorclass"];
+            this.strength = (float)itemData["stats"]["strength"];
+            this.constitution = (float)itemData["stats"]["constitution"];
+            this.dexterity = (float)itemData["stats"]["dexterity"];
+            this.intelligence = (float)itemData["stats"]["intelligence"];
+            this.wisdom = (float)itemData["stats"]["wisdom"];
+            this.charisma = (float)itemData["stats"]["charisma"];
+            //this.sprite= 
+        }
+        catch
+        {
+            this.discription += "\n" + this.id + ": err on load";
+            this.id = "";
+        }
+    }
+}
